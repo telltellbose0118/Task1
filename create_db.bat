22c635eb50040e956221cc949b556f2ac7ae1834
unknown <samuraimkabc@yahoo.co.jp>
2018-04-05 12:22:47 +0900

    create_db.bat fixed

--- create_db.bat
+++ create_db.bat
@@ -1,12 +1,24 @@
 create database db_mk4 default character set utf8;
 grant all privileges on db_db4.* to db_user@localhost with grant option;
 use db_mk4;
-create table bbs(
+
+create table user(
 
 id int unsigned not null auto_increment primary key,
-title varchar(255) not null,
-contents text not null
+name verchar(255) not null,
+password verchar(255) not null,
+
 
 );
 
-insert into bbs (title,contents) values ('first',"first article"), ('second',"second article"), ('third',"third article"), ('fourth',"fourth article");
+create table bbs(
+
+id int unsigned not null auto_increment primary key,
+name verchar(255) not null foreign key (name) references user(name),
+date datetime not null, 
+title varchar(255) not null,
+content text not null
+
+);
+
+insert into bbs (name,date,title,content) values ('user1',now(),'first',"first article"), ('user2',now(),'second',"second article"), ('user3',now(),'third',"third article"), ('user4',now(),'fourth',"fourth article");
